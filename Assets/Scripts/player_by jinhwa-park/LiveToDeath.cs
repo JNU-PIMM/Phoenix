88d6129edf65637de48fd2fae4c6f61f1e40cfb3
jinhwa-park <parkjinhwa3914@gmail.com>
2020-12-27 14:16:24 +0900

    feat : character jump

---
+++ Assets/Scripts/player_by jinhwa-park/LiveToDeath.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveToDeath : MonoBehaviour
+{
+    public GameObject live, death;
+
+    int whichAvatarIsOn = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        live.gameObject.SetActive(true);
+        death.gameObject.SetActive(false);
+    }
+
+    public void SwitchAvatar()
+    {
+//       switch (whi)
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
