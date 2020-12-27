cbdab819002730c8a9ed56c59080c7d58a2a32a6
sieun43 <lsieun0229@gmail.com>
2020-12-27 14:15:29 +0900

    feat : 플레잉 패널

---
+++ Assets/Scripts/system/Gauge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gauge : MonoBehaviour
+{
+    public Image bar;
+    public float maxGauge=100;
+    public float currentGauge=40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bar.fillAmount = currentGauge / maxGauge;
+    }
+}
